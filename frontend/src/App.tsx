f009624dea63e9f0f5740b268730846aa0ae8fc4
evanbene <evanbene48@gmail.com>
2024-05-01 21:34:09 +0700

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <header>TS Amazona</header>
+      Halo
+      <footer>Haha hihi</footer>
+    </div>
+  )
+}
+
+export default App
