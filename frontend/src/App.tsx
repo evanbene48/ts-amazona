b9001c3ccdef2a57913a9c36e30e8f90e009aa00
evanbene <evanbene48@gmail.com>
2024-05-11 15:59:26 +0700

    creating home page and navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,47 @@
-import './App.css'
+import { Button, Col, Container, Nav, Navbar, Row } from 'react-bootstrap'
+import { sampleProducts } from './data'
 
 function App() {
   return (
-    <div>
-      <header>TS Amazona</header>
-      Halo
-      <footer>Haha hihi</footer>
+    <div className="d-flex flex-column vh-100">
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand>TSAmazona</Navbar.Brand>
+          </Container>
+          <div>
+            <Nav>
+              <a href="/cart" className="nav-link">
+                Cart
+              </a>
+              <a href="/signin" className="nav-link">
+                Sign In
+              </a>
+            </Nav>
+          </div>
+        </Navbar>
+      </header>
+      <main>
+        <Container className="mt-3">
+          <Row>
+            {sampleProducts.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3}>
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className="product-image"
+                />
+                <h2>{product.name}</h2>
+                <p>${product.price}</p>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </main>
+      <Button variant="primary">Primary</Button>
+      <footer>
+        <div className="text-center">All rights reserved</div>
+      </footer>
     </div>
   )
 }
